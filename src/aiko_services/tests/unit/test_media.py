4d22e68ccf8d01acb7ee15f68ad9d9ab4a03b536
Nicolas Simonds <github@submedia.net>
2026-07-29 11:29:57 +1000

    fix: Allow VideoSample to use a sample_rate of 0
    If the parameter is set to `0`, act as a "frame sink" and drop all
    frames, rather than trying to divide by zero
    
    Relates: Issue #62

---
+++ src/aiko_services/tests/unit/test_media.py
@@ -0,0 +1,51 @@
+# Usage
+# ~~~~~
+# pytest [-s] unit/test_media.py
+# pytest [-s] unit/test_media.py::test_VideoSample
+
+from typing import Tuple
+
+from PIL import Image
+
+import aiko_services as aiko
+from aiko_services.tests.unit import do_create_pipeline
+
+PIPELINE_DEFINITION = """{
+  "version": 0, "name": "p_test", "runtime": "python",
+  "graph": ["(GenerateImages VideoSample)"],
+  "elements": [
+    { "name":   "GenerateImages",
+      "parameters": {"data_sources": "(file://)"},
+      "input":  [],
+      "output": [{"name": "images", "type": "[image]"}],
+      "deploy": {
+        "local": {"module": "aiko_services.tests.unit.test_media"}
+      }
+    },
+    { "name":   "VideoSample",
+      "input":  [{"name": "images", "type": "[image]"}],
+      "output": [{"name": "images", "type": "[image]"}],
+      "parameters": {"sample_rate": 0},
+      "deploy": {
+        "local": {"module": "aiko_services.elements.media.video_io"}
+      }
+    }
+  ]
+}
+"""
+
+
+class GenerateImages(aiko.DataSource):  # PipelineElement
+    def __init__(self, context: aiko.ContextPipelineElement):
+        self.image = Image.new("RGB", (320, 240), "black")
+        context.set_protocol("generate_images:0")
+        context.call_init(self, "PipelineElement", context)
+
+    def process_frame(self, stream) -> Tuple[aiko.StreamEvent, dict]:
+        aiko.process.terminate()
+        return aiko.StreamEvent.OKAY, {"images": [self.image]}
+
+
+def test_VideoSample(capsys):
+    do_create_pipeline(PIPELINE_DEFINITION)
+    assert "ZeroDivisionError" not in capsys.readouterr().out
